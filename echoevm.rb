221a4abef11b67e52fd6521e01c8eed0e2fc5ee9
smallyunet <dev.wangyu@gmail.com>
2026-08-24 17:15:00 +0800

    echoevm 1.5.0

--- echoevm.rb
+++ echoevm.rb
@@ -1,27 +1,27 @@
 class Echoevm < Formula
   desc "Self-contained Rust EVM debugger, tracer, and replay engine"
   homepage "https://github.com/smallyunet/echoevm"
-  version "1.4.0"
+  version "1.5.0"
   license "MIT"
   head "https://github.com/smallyunet/echoevm.git", branch: "main"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/smallyunet/echoevm/releases/download/v1.4.0/echoevm-darwin-arm64"
-      sha256 "9bc80e3851cd8550a9502324231cbfcf130400f27985b9edea885a475b0b5a69"
+      url "https://github.com/smallyunet/echoevm/releases/download/v1.5.0/echoevm-darwin-arm64"
+      sha256 "d01b277dd374b63a304014ff2fc49edb926a6e719301417bcd538c22b74ff0fa"
     else
-      url "https://github.com/smallyunet/echoevm/releases/download/v1.4.0/echoevm-darwin-amd64"
-      sha256 "62d25921223220d2006e1dfd41bd77cfa1a3e763f880d8ce9d5cee231d3728d4"
+      url "https://github.com/smallyunet/echoevm/releases/download/v1.5.0/echoevm-darwin-amd64"
+      sha256 "ff8299d4a2387be097cc0263e50dd02d8895d7d1801ee70afd660f0536d47261"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/smallyunet/echoevm/releases/download/v1.4.0/echoevm-linux-arm64"
-      sha256 "bba334e4a9c041397270cfb8a43c62ce7d0bf714c8251f34fabe128569492ab4"
+      url "https://github.com/smallyunet/echoevm/releases/download/v1.5.0/echoevm-linux-arm64"
+      sha256 "0864263f44d4f5382808652231ea808382d1f331e75d6337017a3e9822ef204f"
     else
-      url "https://github.com/smallyunet/echoevm/releases/download/v1.4.0/echoevm-linux-amd64"
-      sha256 "0d60faac6a32b29ad6668a37e30ac81913057b65ea3c436de15569a8b19721f1"
+      url "https://github.com/smallyunet/echoevm/releases/download/v1.5.0/echoevm-linux-amd64"
+      sha256 "ef917430a146cd9af93d46e83b1f19dad49039979b70628df2b9d0c3d3b9dcda"
     end
   end
 
@@ -30,7 +30,7 @@ class Echoevm < Formula
   end
 
   test do
-    assert_match '"version":"1.4.0"', shell_output("#{bin}/echoevm version --json")
+    assert_match '"version":"1.5.0"', shell_output("#{bin}/echoevm version --json")
     assert_match '"status": "success"', shell_output("#{bin}/echoevm run 0x600260030100 --json")
   end
 end
