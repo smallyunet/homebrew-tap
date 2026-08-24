f21057f98b8e37fdc66ff8205319662ca08bd047
smallyunet <dev.wangyu@gmail.com>
2026-08-24 21:43:40 +0800

    Update EchoEVM to v1.6.0

--- echoevm.rb
+++ echoevm.rb
@@ -1,27 +1,27 @@
 class Echoevm < Formula
   desc "Self-contained Rust EVM debugger, tracer, and replay engine"
   homepage "https://github.com/smallyunet/echoevm"
-  version "1.5.1"
+  version "1.6.0"
   license "MIT"
   head "https://github.com/smallyunet/echoevm.git", branch: "main"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/smallyunet/echoevm/releases/download/v1.5.1/echoevm-darwin-arm64"
-      sha256 "a08410100dd2d1347432910ab5bfe5870624032d9261ea8db4b6595a298e937d"
+      url "https://github.com/smallyunet/echoevm/releases/download/v1.6.0/echoevm-darwin-arm64"
+      sha256 "611a89734c3c899442aaa637a0a09fee859f0bb57df44598b0daa16bd6438c85"
     else
-      url "https://github.com/smallyunet/echoevm/releases/download/v1.5.1/echoevm-darwin-amd64"
-      sha256 "70601c2ce701333cc95f90668b8ddf97cc548fc7167231fbf32b266d3cf1284b"
+      url "https://github.com/smallyunet/echoevm/releases/download/v1.6.0/echoevm-darwin-amd64"
+      sha256 "5d155a18507545026c5245840e3a3aa659b2bb13f750a49f96dd2b625c56b67d"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/smallyunet/echoevm/releases/download/v1.5.1/echoevm-linux-arm64"
-      sha256 "8f747104448a38e765efea95b60d7b8760e29258f55650f4a0664d3b9fd81ac2"
+      url "https://github.com/smallyunet/echoevm/releases/download/v1.6.0/echoevm-linux-arm64"
+      sha256 "a435aceb9d62eafc2fca3e1203c6f924eb84afd5012c9ad32f22149eac071158"
     else
-      url "https://github.com/smallyunet/echoevm/releases/download/v1.5.1/echoevm-linux-amd64"
-      sha256 "0d17c31d6d1ae9a4b626ef654d7df02ca94001e66697cdd83bd53b66f3e8b492"
+      url "https://github.com/smallyunet/echoevm/releases/download/v1.6.0/echoevm-linux-amd64"
+      sha256 "38c4dba5c82378201a68d2e51627a4e96c45d935940b90b7d47500483f0b7987"
     end
   end
 
@@ -30,7 +30,7 @@ class Echoevm < Formula
   end
 
   test do
-    assert_match '"version":"1.5.1"', shell_output("#{bin}/echoevm version --json")
+    assert_match '"version":"1.6.0"', shell_output("#{bin}/echoevm version --json")
     assert_match '"status": "success"', shell_output("#{bin}/echoevm run 0x600260030100 --json")
   end
 end
